a193a21c0bd559478ba43b112dc0d64dcc4fb2ae
yvonnekwchan <yvonnekwchan@gmail.com>
2025-03-18 23:35:29 +0800

    Add .env

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: "http://localhost:5000/api/",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/"
   });
 }
